ddf08d40346f719e6af9eb9b065145d07ebf1439
Marius Egede Knudsen <mak004@edu.zealand.dk>
2024-09-16 14:23:11 +0200

    forgot to push incrementally, whoopise

---
+++ CalculatorClassLibrary/Calculator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorClassLibrary
+{
+    /// <summary>
+    /// Class library for calculating operations
+    /// </summary>
+    public static class Calculator
+    {
+        /// <summary>
+        /// Adds two whole numbers together
+        /// </summary>
+        /// <param name="x">Number 1 to add</param>
+        /// <param name="y">Number 2 to add</param>
+        /// <returns>The result of the addition</returns>
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        /// <summary>
+        /// Adds decimal numbers together
+        /// </summary>
+        /// <param name="x">Number 1 to add</param>
+        /// <param name="y">Number 2 to add</param>
+        /// <returns>The result of the addition</returns>
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        /// <summary>
+        /// Subtracts two whole numbers from eachother
+        /// </summary>
+        /// <param name="x">The number to subtract from</param>
+        /// <param name="y">The number that will be subtracted from x</param>
+        /// <returns></returns>
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        /// <summary>
+        /// Subtracts decimal numbers from eachother
+        /// </summary>
+        /// <param name="x">The number to subtract from</param>
+        /// <param name="y">The number that will be subtracted from x</param>
+        /// <returns></returns>
+        public static double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+        /// <summary>
+        /// Multiplies two whole numbers
+        /// </summary>
+        /// <param name="x">Number 1 to multiply</param>
+        /// <param name="y">Number 2 to multiply</param>
+        /// <returns></returns>
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        /// <summary>
+        /// Multiplies decimal numbers
+        /// </summary>
+        /// <param name="x">Number 1 to multiply</param>
+        /// <param name="y">Number 2 to multiply</param>
+        /// <returns></returns>
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        /// <summary>
+        /// Divides decimal numbers
+        /// </summary>
+        /// <param name="x">Number to be divided</param>
+        /// <param name="y">Number to divide by</param>
+        /// <returns></returns>
+        public static double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ CalculatorClassLibraryTests/CalculatorTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorClassLibrary.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        [DataTestMethod()]
+        [DataRow(0, 5, 5)]
+        [DataRow(50, -20, 30)]
+        [DataRow(0, 0, 0)]
+        [DataRow(-10, -10, -20)]
+        public void AddTestInt(int num1, int num2, int expected)
+        {
+            int result = Calculator.Add(num1, num2);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        [DataTestMethod()]
+        [DataRow(20.5, 10.5, 31)]
+        [DataRow(50.1, -20.1, 30)]
+        [DataRow(0, 0, 0)]
+        [DataRow(-20.5, -10.4, -30.9)]
+        public void AddTestDouble(double num1, double num2, double expected)
+        {
+            double result = Calculator.Add(num2, num1);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        [DataTestMethod()]
+        [DataRow(20, 10, 10)]
+        [DataRow(50, -20, 70)]
+        [DataRow(0, 0, 0)]
+        [DataRow(-20, -10, -10)]
+        public void SubtractTestsInt(int num1, int num2, int expected)
+        {
+            double result = Calculator.Subtract(num1, num2);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        [DataTestMethod()]
+        [DataRow(20.65, 10.65, 10)]
+        [DataRow(25.214, 10.553, 14.661)]
+        [DataRow(0, 0, 0)]
+        [DataRow(10.5, 10, 0.5)]
+        [DataRow(-20.456, -10.123, -10.333)]
+        public void SubtractTestsDouble(double num1,double num2, double expected)
+        {
+            double result = Calculator.Subtract(num1, num2);
+            Assert.AreEqual(expected, result, 0.0001);
+        }
+        [TestMethod()]
+        [DataTestMethod()]
+        [DataRow(100, 10, 1000)]
+        [DataRow(0, 1, 0)]
+        [DataRow(0, 0, 0)]
+        [DataRow(10, 5, 50)]
+        [DataRow(-10, 5, -50)]
+        [DataRow(-10, -10, 100)]
+        public void MultipylTestsInt(int num1, int num2, int expected)
+        {
+            int result = Calculator.Multiply(num1, num2);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        [DataTestMethod()]
+        [DataRow(100.5, 10.1, 1015.05)]
+        [DataRow(0, 1.6, 0)]
+        [DataRow(0, 0, 0)]
+        [DataRow(10.1, 5.15, 52.015)]
+        [DataRow(-10.5, 5.10, -53.55)]
+        [DataRow(-10, -10, 100)]
+        public void MultipylTestsDouble(double num1, double num2, double expected)
+        {
+            double result = Calculator.Multiply(num1, num2);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        [DataTestMethod()]
+        [DataRow(5, 2, 2.5)]
+        [DataRow(1, 4, 0.25)]
+        [DataRow(10, 3, 3.33333333)]
+        [DataRow(50, 10, 5)]
+        [DataRow(-10, -20, 0.5)]
+        public void DivideTestsDouble(double num1, double num2, double expected)
+        {
+            double result = Calculator.Divide(num1, num2);
+            Assert.AreEqual(expected, result, 0.001);
+        }
+    }
+}
